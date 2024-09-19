48a22a9ec795ee9d510c2ca384559ad7db5e183c
Valène Renaud <valene.renaud@gmail.com>
2024-09-19 14:55:56 +0200

    feat(exercice3): implement calculateAverage function to calculate the average of an array and handle empty array

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,4 +1,21 @@
-// Créez votre fonction ici
+/**
+ * Calcule la moyenne des nombres dans un tableau
+ * @param {number[]} numbers Un tableau de nombres
+ * @returns {number|string} La moyenne des nombres ou un message d'erreur si le tableau est vide
+ */
+function calculateAverage(numbers = []) {
+  // Vérifie si le tableau est vide 
+  if (numbers.length === 0) {
+    return 'No numbers to calculate average';
+  }
+
+  // Calcule la somme de tous les éléments du tableau
+  const sum = numbers.reduce((accumulatorSum, currentValue) => accumulatorSum + currentValue, 0);
+
+  // Retourne la moyenne en divisant la somme par la longueur du tableau
+  return sum / numbers.length;
+}
+
 
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10

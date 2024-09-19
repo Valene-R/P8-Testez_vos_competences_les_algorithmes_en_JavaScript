2dd2839c4d0bde5b7115ecfbb4e230c964e31f1a
Valène Renaud <valene.renaud@gmail.com>
2024-09-19 13:56:23 +0200

    feat(exercice1): implement pairNumbers function to return even numbers between min and max

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,21 @@
-//votre code ici
+/**
+ * Retourne une chaîne de caractères contenant tous les nombres pairs 
+ * entre un minimum et un maximum, séparés par une virgule (sans espace)
+ * @param {number} min Le nombre minimum
+ * @param {number} max Le nombre maximum
+ * @returns {string} Une chaîne de caractères avec les nombres pairs
+ */
+function pairNumbers(min, max) {
+  let result = [];
+
+  // Parcourt tous les nombres de min à max
+  for (let i = min; i <= max ; i++) {
+    // Vérifie si le nombre est pair
+    if (i % 2 === 0) {
+      result.push(i);
+  }
+}
+return result.join(',');
+}
 
 export default pairNumbers
